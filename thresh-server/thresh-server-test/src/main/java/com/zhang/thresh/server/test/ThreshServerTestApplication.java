9bdfc448ae234bec85b08ee3c1c174ccdc6b745d
zhangtiancheng <1127152834@qq.com>
2021-12-26 23:23:05 +0800

    遺漏文件

---
+++ thresh-server/thresh-server-test/src/main/java/com/zhang/thresh/server/test/ThreshServerTestApplication.java
@@ -0,0 +1,23 @@
+package com.zhang.thresh.server.test;
+
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import com.zhang.thresh.common.security.starter.annotation.EnableThreshCloudResourceServer;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableThreshCloudResourceServer
+@EnableTransactionManagement
+@EnableDistributedTransaction
+@MapperScan("com.zhang.thresh.server.test.mapper")
+public class ThreshServerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreshServerTestApplication.class, args);
+    }
+
+}
