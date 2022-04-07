e89249fae45a686b8918c9d102e90a566ca226b2
bisicloud <bisicloud@163.com>
2022-04-07 17:28:51 +0800

    添加nacos

---
+++ thresh-server/thresh-server-scanner/src/main/java/com/zhang/thresh/server/scanner/ThreshServerScannerApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.thresh.server.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreshServerScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreshServerScannerApplication.class, args);
+    }
+
+}
